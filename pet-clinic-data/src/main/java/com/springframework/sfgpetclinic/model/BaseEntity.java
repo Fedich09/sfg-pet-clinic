dbe3aa8ff502122835831882bad3c3edcc68cb19
mfedenko <mfedenko@office.atlassian.com>
2021-10-27 22:33:21 +0300

    added lombok to the project. New comment here !!! Closes #77

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,22 +1,27 @@
 package com.springframework.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
+
+
--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,10 @@
 package com.springframework.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
@@ -9,6 +14,10 @@ import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -17,12 +26,4 @@ public class Vet extends Person {
     @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
             inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities = new HashSet<>();
-
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
