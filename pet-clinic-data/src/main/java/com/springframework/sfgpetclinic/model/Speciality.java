5c1a8ac60d8be943742d8003ad3e924e7fa5db30
mfedenko <mfedenko@office.atlassian.com>
2021-10-27 22:33:21 +0300

    added lombok to the project. Closes #77

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Speciality.java
@@ -1,21 +1,26 @@
 package com.springframework.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "speciality")
 public class Speciality extends BaseEntity {
 
     @Column(name = "description")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
